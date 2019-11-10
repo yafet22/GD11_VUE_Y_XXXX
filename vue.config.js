fd768d697d3ec901bf58c0e94e24321b6dcc3044
Yafet Trakan T <yafet.trakan@gmai.com>
2019-11-10 13:24:37 +0700

    my first commit

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // devServer: {
+  //   proxy: 'http://127.0.0.1:81/modul11/index.php' //lokasi folder project CI kalian di localhost
+  // },
+  lintOnSave: false,
+  "transpileDependencies": [
+    "vuetify"
+  ],
+}
